bf649ba6e60ba5ba125c1cf14394dd386b63bd85
jyokota <jyokota@r6.ucom.ne.jp>
2019-02-03 18:24:44 +0900

    ScreenBattle追加

---
+++ Assets/DataCharacter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterParam
+{
+    int id;
+    int type;
+    string name;
+    int hp;
+    int mp;
+
+}
+
+
+public class DataCharacter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
