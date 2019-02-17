14046d669c07f1669ec013f26e07908ca4004b35
jyokota <jyokota@r6.ucom.ne.jp>
2019-02-17 17:17:39 +0900

    コマンドUI

---
+++ Assets/UserDataManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character
+{
+    public int id;
+    public string name;
+    public int hp;
+    public int wp;
+}
+
+public class UserDataManager : SingletonMonoBehaviour<UserDataManager>
+{
+    private List<Character> deckList = new List<Character>();
+    public List<Character> DeckList { get { return deckList; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
